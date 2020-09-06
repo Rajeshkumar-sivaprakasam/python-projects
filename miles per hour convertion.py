a76a20dca79ea493310a6ddecd2c30b258975b97
Rajesh <infothamizha@gmail.com>
2020-09-06 08:28:33 +0530

    Miles per hour added

---
+++ miles per hour convertion.py
@@ -0,0 +1,13 @@
+
+
+print("Welcoome to Miles per hour app")
+print()
+
+mph = float(input("What is your speed in miles per hour:\t "))
+print()
+
+mps = mph * 0.4474
+
+mps = round(mps,2)
+print(f"Your speed in meters per second is {mps}.")
+
